297458c8fa291172bd5b6577a23c55852fae1bf8
Ricky Hougland <houglande@gmail.com>
2017-12-05 22:52:40 -0800

    Day 6 cleaning up a little

--- src/day6/Day6.java
+++ src/day6/Day6.java
@@ -8,73 +8,61 @@ import java.util.stream.Stream;
 
 public class Day6 {
     public static void main(String[] args) throws IOException {
-        List<Integer> input = parseInput("src/day6/input.txt");
+        final List<Integer> input = parseInput("src/day6/input.txt");
 
         int cycles = 0;
         boolean foundDup = false;
         boolean foundDupAgain = false;
-        boolean setDup = false;
-        List<Integer> dup = new ArrayList<>();
+        final List<Integer> dup = new ArrayList<>();
         final Set<List<Integer>> seen = new HashSet<>();
 
-        while (!foundDup || !foundDupAgain) {
+        while (!(foundDup && foundDupAgain)) {
             cycles++;
-            input = redistribute(input);
+            redistribute(input);
 
             if (input.equals(dup)) {
                 foundDupAgain = true;
-                continue;
+                System.out.println("Saw input equalling dup at [" + cycles + "] num cycles");
             }
 
             if (seen.contains(input)) {
-                if (!setDup) {
-                    System.out.println(cycles);
-
+                if (dup.size() == 0) {
+                    System.out.println("Saw first dup at [" + cycles + "] num cycles");
                     dup.addAll(input);
-                    setDup = true;
                 }
                 foundDup = true;
             }
 
             seen.add(input);
         }
-
-        System.out.println("final " + cycles);
     }
 
-    private static List<Integer> redistribute(
+    private static void redistribute(
             final List<Integer> banks
     ) {
-        final List<Integer> newBanks = banks;
-
         final int max = Collections.max(banks);
 
         int indexOfMax = banks.indexOf(max);
 
-        newBanks.set(indexOfMax, 0);
+        banks.set(indexOfMax, 0);
 
-        int currIndex;
-        if (indexOfMax + 1 == newBanks.size()) {
-            currIndex = 0;
-        } else {
+        int currIndex = 0;
+        if (!(indexOfMax + 1 == banks.size())) {
             currIndex = indexOfMax + 1;
         }
 
         for (int i = 0; i < max; i++) {
-            int value = newBanks.get(currIndex);
+            int value = banks.get(currIndex);
 
-            newBanks.set(currIndex, value + 1);
+            banks.set(currIndex, value + 1);
 
-            if (currIndex + 1 == newBanks.size()) {
+            if (currIndex + 1 == banks.size()) {
                 currIndex = 0;
             } else {
                 currIndex++;
             }
         }
-
-        return newBanks;
     }
-
 
     private static List<Integer> parseInput(final String filename) throws IOException {
         List<Integer> parsedInput = new ArrayList<>();
@@ -82,14 +70,11 @@ public class Day6 {
         final Stream<String> lines = Files.lines(Paths.get(filename));
 
         lines.forEach(line -> {
-                    System.out.println(line);
-                    String[] splitLine = line.split("\\t");
-
-                    Arrays.stream(splitLine)
-                            .map(Integer::valueOf)
-                            .forEach(item -> parsedInput.add(item));
-        }
-        );
+            String[] splitLine = line.split("\\t");
+            Arrays.stream(splitLine)
+                    .map(Integer::valueOf)
+                    .forEach(parsedInput::add);
+        });
 
         return parsedInput;
     }
